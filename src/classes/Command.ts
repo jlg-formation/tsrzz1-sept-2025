96478f83da7cec60613bce4a9a16a539e42a3bb8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-09-30 14:01:02 +0200

    dev: added render

--- src/classes/Command.ts
+++ src/classes/Command.ts
@@ -1,4 +1,5 @@
 import type { Config } from "../types/Config";
+import { querySelector } from "../utils/querySelector";
 
 type Callback = (newConfig: Config) => void;
 
@@ -10,8 +11,34 @@ export class Command {
 
   private callback: Callback = () => {};
 
+  constructor() {
+    this.render();
+  }
+
+  /**
+   * Affiche le visuel qui correspond à l'état du composant
+   */
+  render() {
+    const elt = querySelector(".command .samples .value");
+    elt.innerHTML = this.config.samples + "";
+    const elt2 = querySelector(".command .multiplicationFactor .value");
+    elt2.innerHTML = this.config.multiplicationFactor + "";
+
+    const sliderElt = querySelector(
+      ".command .samples input",
+      HTMLInputElement
+    );
+    sliderElt.value = this.config.samples + "";
+    const sliderElt2 = querySelector(
+      ".command .multiplicationFactor input",
+      HTMLInputElement
+    );
+    sliderElt2.value = this.config.multiplicationFactor + "";
+  }
+
   setConfig(config: Config) {
     this.config = config;
+    this.render();
   }
 
   onChange(callback: Callback) {
--- src/utils/querySelector.ts
+++ src/utils/querySelector.ts
@@ -1,7 +1,15 @@
-export const querySelector = (selector: string) => {
+export const querySelector = <T extends Element>(
+  selector: string,
+  type?: new () => T
+): T => {
   const elt = document.querySelector(selector);
   if (elt === null) {
     throw new Error(`Element avec selecteur '${selector}' non trouvé.`);
   }
-  return elt;
+  if (type && !(elt instanceof type)) {
+    throw new Error(
+      `Element ${selector} trouvé mais le type n'est pas bon (${type})`
+    );
+  }
+  return elt as T;
 };
